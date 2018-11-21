1db77ba83113a399326bfb7295629825118f45ae
billy <sangyounw@naver.com>
2018-11-21 20:52:23 +0900

    Front-firstpage-survey..

---
+++ src/components/survey/SupplierSurvey.js
@@ -0,0 +1,4 @@
+                
+                // <i class="china flag"></i>
+                // <i class="india flag"></i>
+                // <i class="indonesia flag"></i>
--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+// import React,{Fragment} from 'react';
+// import ReactDOM from 'react-dom';
+// import FirstPage from './components/FirstPage';
+// import BuyerSurvey from './components/BuyerSurveyOne';
+
+// class App extends React.Component{
+
+//     state = {
+
+//     };
+
+//     render(){
+//         return (
+//             <Fragment>
+//                 <FirstPage/>
+//             </Fragment>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<BuyerSurvey/>, document.querySelector("#root"));
+
