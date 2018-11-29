0b0ab06673943672a3b2a73109255a79d4fe4b0c
pseudotop <lsupertopl@gmail.com>
2018-11-29 17:47:03 +0900

    Add redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-
 import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
 import App from './components/App';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 const Root = () => (
   <BrowserRouter>
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <App/>
     </Provider>
   </BrowserRouter>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,10 @@
 import { combineReducers } from 'redux';
-
-const usersReducer = (action) => {
-  return [
-    { email: 'lsupertopl@gmail.com', password: '1234', firstName: 'Byoungjun', lastName: 'Kim'},
-    { email: 'admin@gmail.com', password: '1234', firstName: 'Admin', lastName: 'Admin'},
-  ];
-};
-
-const loggedInUserReducer = (loggedInUser=null, action) => {
-  if (action.type === 'USER_LOGGEDIN') {
-    return action.payload;
-  } else {
-    return loggedInUser;
-  }
-};
+import { alert } from './alert.reducer';
+import { authentication } from './authentication.reducer';
+import { users } from './users.reducer';
 
 export default combineReducers({
-  users: usersReducer,
-  loggedInUser: loggedInUserReducer
+  users: users,
+  auth: authentication,
+  alert: alert,
 });
