ac56e072ab030d34c9652a7b43d1139ab2719684
Billy-Kimm <sangyounw@naver.com>
2018-12-05 14:12:42 +0900

    1205

---
+++ client/src/components/transaction/RegProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/OnTransaction.css';
+
+const RegProduct = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegProduct;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-
-import FirstPage from './components/survey/FirstPage';
-import BuyerSurveyOne from './components/survey/BuyerSurveyOne';
-import BuyerSurveyTwo from './components/survey/BuyerSurveyTwo';
-import BuyerSurveyThree from './components/survey/BuyerSurveyThree';
-import OnTransaction from './components/transaction/OnTransaction';
-import AboutProducer from './components/transaction/AboutProducer';
-
-class App extends React.Component{
-
-    state = {
-
-    };
-
-    render(){
-        return (
-            <Fragment>
-                {/* <FirstPage/> */}
-                {/* <BuyerSurveyOne/> */}
-                 {/* <BuyerSurveyTwo/> */}
-                {/* <BuyerSurveyThree/> */}
-                {/* <OnTransaction/> */}
-                {/* <AboutProducer/> */}
-            </Fragment>
-        );
-    }
-}
-
-ReactDOM.render(<BuyerSurveyOne/>, document.querySelector("#root"));
-=======
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -48,5 +17,4 @@ const Root = () => (
 );
 
 ReactDOM.render(<Root/>, document.querySelector("#root"));
->>>>>>> ad40e2dc3f67462e4921d9842dd20ac92ec6185f
 
