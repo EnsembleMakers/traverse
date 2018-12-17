478639c7f8b215ec9587c814a753abc10733f30f
pseudotop <lsupertopl@gmail.com>
2018-12-17 16:28:04 +0900

    Add jwt and google proxy

--- server/routes/middlewares.js
+++ server/routes/middlewares.js
@@ -1,4 +1,4 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 // const RateLimit = require('express-rate-limit');
 const bcrypt = require('bcrypt');
 
@@ -31,23 +31,23 @@ exports.hashPassword = async (password) => {
   return hashedPassword;
 }
 
-// exports.verifyToken = (req, res, next) => {
-//   try {
-//     req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-//     return next();
-//   } catch (error) {
-//     if (error.name === 'TokenExpiredError') { // 유효기간 초과
-//       return res.status(419).json({
-//         code: 419,
-//         message: '토큰이 만료되었습니다',
-//       });
-//     }
-//     return res.status(401).json({
-//       code: 401,
-//       message: '유효하지 않은 토큰입니다'
-//     });
-//   }
-// };
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') { // 유효기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다'
+    });
+  }
+};
 
 // exports.apiLimiter = new RateLimit({
 //   windowMs: 60 * 1000, // 1분
